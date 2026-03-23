7e7341649cce961a6f50730455e31c36df188b25
gesimorris <t00686038@mytru.ca>
2026-03-22 21:49:02 -0700

    render update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://your-backend-name.onrender.com' // Your Render URL
+  ? 'https://movie-recommender-system-lmwa.onrender.com' // Your Render URL
   : 'http://127.0.0.1:5000';                 // Your Local URL
 
 export default API_BASE_URL;
