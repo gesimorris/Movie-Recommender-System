c677d44b904eb06554ed3bd961c434d33f532683
gesimorris <t00686038@mytru.ca>
2026-03-30 23:07:07 -0700

    vercel deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
-// src/config.js
-const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://movie-recommender-system-lmwa.onrender.com' // Your Render URL
-  : 'http://127.0.0.1:5000';                 // Your Local URL
 
-export default API_BASE_URL;
+const API_URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : process.env.REACT_APP_API_URL;
+
+export default API_URL;
